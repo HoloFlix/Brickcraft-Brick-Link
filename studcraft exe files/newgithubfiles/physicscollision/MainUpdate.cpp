baf585884f7387196ca237d5db5298c3d0e2fda1
Shortygotsauce <paxnotpacks@gmail.com>
2025-07-16 11:37:01 +0800

    Delete studcraft exe files directory

--- studcraft exe files/newgithubfiles/physicscollision/MainUpdate.cpp
+++
@@ -1,9 +0,0 @@
-// Each frame:
-physicsWorld.Step(deltaTime);
-
-// To sync player position for rendering:
-btTransform trans;
-playerPhysics.GetBody()->getMotionState()->getWorldTransform(trans);
-player.position.x = trans.getOrigin().getX();
-player.position.y = trans.getOrigin().getY();
-player.position.z = trans.getOrigin().getZ();
